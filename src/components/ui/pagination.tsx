efc9664a4c8065bc010f6705946adfe544033466
Michał Kidawa <mkidawa1@gmail.com>
2025-05-06 00:17:20 +0200

    fix: update pagination labels

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -58,7 +58,7 @@ function PaginationPrevious({ className, ...props }: React.ComponentProps<typeof
       {...props}
     >
       <ChevronLeftIcon />
-      <span className="hidden sm:block">Previous</span>
+      <span className="hidden sm:block">Poprzednia</span>
     </PaginationLink>
   );
 }
@@ -71,7 +71,7 @@ function PaginationNext({ className, ...props }: React.ComponentProps<typeof Pag
       className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
       {...props}
     >
-      <span className="hidden sm:block">Next</span>
+      <span className="hidden sm:block">Następna</span>
       <ChevronRightIcon />
     </PaginationLink>
   );
@@ -86,7 +86,7 @@ function PaginationEllipsis({ className, ...props }: React.ComponentProps<"span"
       {...props}
     >
       <MoreHorizontalIcon className="size-4" />
-      <span className="sr-only">More pages</span>
+      <span className="sr-only">Więcej stron</span>
     </span>
   );
 }
